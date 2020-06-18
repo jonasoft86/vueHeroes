426d65ef3b9dba0231bf6e38fe24fe112c7a7191
Jonathan <jonathan.monroy@live.com>
2020-06-18 00:25:27 -0400

    Se agrega Router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -17,5 +18,6 @@ Vue.use(BootstrapVue)
 axios.defaults.baseURL = 'http://157.245.138.232:9091/api/v1/test';
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
